24caceeab601a73d5c4e70b635fadb24c1b8aee2
Narravula Prem Sai <76964907+narravulapremsai@users.noreply.github.com>
2024-12-09 10:09:25 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/library/model/Book.java
@@ -0,0 +1,42 @@
+package com.library.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+    private boolean isIssued;
+
+    public Book(int id, String title, String author, String category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.isIssued = false;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void setIssued(boolean issued) {
+        isIssued = issued;
+    }
+}
